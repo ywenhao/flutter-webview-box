f68619ed4ab999739f43183276ad961a21a64aab
yuanwenhao <actions.win@foxmail.com>
2025-05-26 15:23:47 +0800

    feat: 调整

--- lib/main.dart
+++ lib/main.dart
@@ -16,24 +16,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
@@ -111,12 +93,14 @@ class _MyHomePageState extends State<MyHomePage> {
       return;
     }
 
+    final method = params["name"];
+    final args = params["args"];
+
     // 直接运行， 没有返回值
     if (cbId == null) {
+      // todo
     } else {
       // 有返回值， 异步调用
-      final method = params["name"];
-      final args = params["args"];
       try {
         callJsResolve(cbId, {
           "args": args,
